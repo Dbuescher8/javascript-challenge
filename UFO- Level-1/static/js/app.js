7f1428e8021264bce9a2d5854b14eaf94b1598b0
Dbuescher8 <buescher26@ymail.com>
2021-04-27 20:52:32 -0500

    added files

--- UFO- Level-1/static/js/app.js
+++ UFO- Level-1/static/js/app.js
@@ -1,4 +1,26 @@
-// from data.js
 var tableData = data;
+// your code here
+var tbody = d3.select("tbody");
+tableData.forEach(function(ufo){
+    var row = tbody.append("tr");
+    Object.entries(ufo).forEach(function([key,value]){
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
 
-// YOUR CODE HERE!
+var button = d3.select("#filter-btn");
+button.on("click",function(event){
+    d3.event.preventDefault();
+    tbody.html("");
+    var filterDate = d3.select("#datetime");
+    var filterValue = filterDate.property("value");
+    var filterData = tableData.filter(tableData => tableData.datetime === filterValue);
+    filterData.forEach(function(ufoDate){
+        var row = tbody.append("tr");
+        Object.entries(ufoDate).forEach(function([key,value]){
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});
