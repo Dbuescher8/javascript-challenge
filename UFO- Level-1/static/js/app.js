b9be30c976e808918e69a6c9fe31ec65ba817861
Dbuescher8 <buescher26@ymail.com>
2021-04-27 20:56:47 -0500

    added files

--- UFO- Level-1/static/js/app.js
+++ UFO- Level-1/static/js/app.js
@@ -1,26 +1,60 @@
+// from data.js
 var tableData = data;
-// your code here
-var tbody = d3.select("tbody");
-tableData.forEach(function(ufo){
-    var row = tbody.append("tr");
-    Object.entries(ufo).forEach(function([key,value]){
-        var cell = row.append("td");
-        cell.text(value);
-    });
-});
 
+// YOUR CODE HERE!
+var tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
-button.on("click",function(event){
-    d3.event.preventDefault();
-    tbody.html("");
-    var filterDate = d3.select("#datetime");
-    var filterValue = filterDate.property("value");
-    var filterData = tableData.filter(tableData => tableData.datetime === filterValue);
-    filterData.forEach(function(ufoDate){
-        var row = tbody.append("tr");
-        Object.entries(ufoDate).forEach(function([key,value]){
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });
-});
+var inputField1 = d3.select("#datetime");
+var inputField2 = d3.select("#city");
+var resetbtn = d3.select("#reset-btn");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+var populate = (dataInput) => {
+
+	dataInput.forEach(ufo_sightings => {
+		var row = tbody.append("tr");
+		columns.forEach(column => row.append("td").text(ufo_sightings[column])
+		)
+	});
+}
+
+//Populate table
+populate(data);
+
+// Filter by attribute
+button.on("click", () => {
+	d3.event.preventDefault();
+	var inputDate = inputField1.property("value").trim();
+	var inputCity = inputField2.property("value").toLowerCase().trim();
+	// Filter by field matching input value
+	var filterDate = data.filter(data => data.datetime === inputDate);
+	console.log(filterDate)
+	var filterCity = data.filter(data => data.city === inputCity);
+	console.log(filterCity)
+	var filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity);
+	console.log(filterData)
+
+	// Add filtered sighting to table
+	tbody.html("");
+
+	let response = {
+		filterData, filterCity, filterDate
+	}
+
+	if (response.filterData.length !== 0) {
+		populate(filterData);
+	}
+		else if (response.filterData.length === 0 && ((response.filterCity.length !== 0 || response.filterDate.length !== 0))){
+			populate(filterCity) || populate(filterDate);
+	
+		}
+		else {
+			tbody.append("tr").append("td").text("No results found!"); 
+		}
+})
+
+resetbtn.on("click", () => {
+	tbody.html("");
+	populate(data)
+	console.log("Table reset")
+})
